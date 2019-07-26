9f3ce7be95646bbcf79f83370d00d4b344eb2cb0
Cristiano Ramos <limaramos.c@gmail.com>
2019-07-25 22:53:18 -0300

    acrentando a mensagem desejando um bom dia!

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -10,9 +10,9 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá mundo!");
-            Console.WriteLine("bom dia");
-            Console.WriteLine("como vc esta");
+            Console.WriteLine("Olá mundo!");          
+            Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um bom dia!");
             Console.ReadLine();
         }
     }
